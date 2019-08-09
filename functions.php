da59995fb29de853c8d3fb9cd69669762549ca70
jomurgel <jo@alley.co>
2019-08-09 07:47:02 -0600

    Replaces defunct $tweet->age( $tweet ) with tapi_get_relative_time function

--- functions.php
+++ functions.php
@@ -152,7 +152,7 @@ function tapi_get_timestamp( $format = 'raw', $tweet = false ) {
 		return $timestamp;
 
 	if ( 'age' == $format )
-		return $tweet->age( $tweet );
+		return tapi_get_relative_time( $tweet->created_at );
 
 	return date( $format, $timestamp );
 }
@@ -160,5 +160,60 @@ function tapi_get_timestamp( $format = 'raw', $tweet = false ) {
 
 function tapi_get_permalink( $tweet = false ) {
 	return "https://twitter.com/" . tapi_get_author_screen_name( $tweet ) . '/status/' . tapi_get_tweet_id( $tweet );
+}
+
+/**
+ * Return relative time string from unix timestamp.
+ *
+ * @param string || int timestamp .ex Fri Aug 09 00:59:06 +0000 2019
+ */
+function tapi_get_relative_time( $timestamp = '' ) {
+
+	// Bail if no timestamp exists.
+	if ( empty( $timestamp ) ) {
+		return;
+	}
+
+	// Setup new time.
+	$now  = new \DateTime();
+	$ago  = new \DateTime( $timestamp );
+	$diff = $now->diff( $ago );
+
+	// Round week down to nearest int.
+	$diff->w = intval( floor( $diff->d / 7 ) );
+
+	// Subtract day from week and set to var.
+	$diff->d -= $diff->w * 7;
+
+	// Set potential string output to array.
+	$time_string_array = array(
+		'y' => 'year',
+		'm' => 'month',
+		'w' => 'week',
+		'd' => 'day',
+		'h' => 'hour',
+		'i' => 'minute',
+		's' => 'second',
+	);
+
+	// Loop through each time.
+	foreach ( $time_string_array as $key => &$time ) {
+		// Set $spacetime key as plural,
+		// else remove key from array if key isn't valid.
+		if ( $diff->$key ) {
+			$time = $diff->$key . ' ' . $time . ( $diff->$key > 1 ? 's' : '' );
+		} else {
+			unset( $time_string_array[ $key ] );
+		}
+	}
+
+	// Get first element from array.
+	$time_space = array_slice( $time_string_array, 0, 1 );
+
+	// If we have a value assume past, and attache 'ago', else just now.
+	$time_string = $time_space ? implode( ', ', $time_space ) . esc_html__( ' ago', 'tapi_tweets' ) : esc_html__( 'just now', 'tapi_tweets' );
+
+	// Output markup.
+	return $time_string;
 }
 
